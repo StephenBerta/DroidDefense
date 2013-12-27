5eac509ae0b1a31e9b5f0cd0869b2e93789e21d9
StephenBertaHome <stephenkberta@gmail.com>
2013-12-26 22:46:16 -0800

    added screens

--- src/com/example/droiddefense/MainMenu.java
+++ src/com/example/droiddefense/MainMenu.java
@@ -1,8 +1,10 @@
 package com.example.droiddefense;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainMenu extends Activity {
 
@@ -10,6 +12,8 @@ public class MainMenu extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
+		
+		
 	}
 
 	@Override
@@ -17,6 +21,12 @@ public class MainMenu extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
+	}
+	
+	public void startOptions(View view) {
+		//create intent to go to options
+		Intent intent = new Intent(this, OptionsSettings.class);
+		startActivity(intent);
 	}
 
 }
